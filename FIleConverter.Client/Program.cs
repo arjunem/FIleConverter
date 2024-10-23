8f94fad7ecc7fda1a23be9295549acc8b684abd4
Arjun Mukundan <arjunmukundan@outlook.in>
2024-10-23 17:31:42 +0530

    Added test and config file to client

--- FIleConverter.Client/Program.cs
+++ FIleConverter.Client/Program.cs
@@ -1,15 +1,46 @@
-﻿namespace FileConverter.Client
+﻿using FIleConverter.Client;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+
+namespace FileConverter.Client
 {
     class Program
     {
+        private static string? serverIp;
+        private static int? port;
         static void Main(string[] args)
         {
+
+            // Build configuration
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .Build();
+
+            // Define the path to your config.json file
+            string configFilePath = Path.Combine(AppContext.BaseDirectory, "config.json");
+
+            // Read the content of the config.json file
+            string json = File.ReadAllText(configFilePath);
+
+            // Deserialize the JSON content into C# objects
+            var config = JsonConvert.DeserializeObject<AppConfig>(json);
+
             // Set the folder to monitor
-            string folderToWatch = @"D:\Learn\FIleConverter\Files"; // Change to the folder you want to monitor
+            var folderToWatch = config.ApplicationSettings.FolderToWatch ?? configuration["ApplicationSettings:FolderToWatch"]; // Change to the folder you want to monitor
+
+            serverIp = config.ApplicationSettings.ServerIP?? configuration["ApplicationSettings:ServerIP"];
+            port = config.ApplicationSettings.Port;
+            if (serverIp == null)
+            {
+                if(int.TryParse(configuration["ApplicationSettings:Port"]?.ToString(), out int output)){
+                    port = output;
+                }
+            }
 
             // Create a FileSystemWatcher to monitor the directory
             FileSystemWatcher watcher = new FileSystemWatcher();
-            watcher.Path = folderToWatch;
+            watcher.Path =  folderToWatch;
 
             // Watch for changes in the creation of new files
             watcher.NotifyFilter = NotifyFilters.FileName | NotifyFilters.CreationTime;
@@ -25,7 +56,7 @@
 
             Console.WriteLine($"Monitoring folder: {folderToWatch}. Press 'q' to quit.");
 
-            TCPFileClient.ReadHeartBeat();
+            TCPFileClient.ReadHeartBeat(serverIp, port);
 
             // Keep the console app running until the user quits
             while (Console.Read() != 'q') ;
@@ -35,10 +66,7 @@
         private static void OnNewFileAdded(object sender, FileSystemEventArgs e)
         {
             Thread.Sleep(100);
-
-            // Trigger your console application here
-            TCPFileClient.SendFile(e.FullPath);
-            //TriggerConsoleApp(e.FullPath);
+            TCPFileClient.SendFile(e.FullPath,serverIp,port);
         }
     }
 }
